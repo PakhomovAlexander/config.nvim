04895f6ff7ec141eee89cbfddb368a6b94fcefd5
apakhomov <apkhmv@gmail.com>
2024-02-25 19:01:24 +0300

    Add more options

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,1 +1,10 @@
-vim.api.nvim_set_keymap('n', '<esc><esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<esc><esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+
+-- Move highlighted blocks with J and K
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+
