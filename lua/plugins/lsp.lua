2ad30da0d710ae9e87aa4804687f8aefd52b088e
apakhomov <apkhmv@gmail.com>
2024-04-18 23:12:14 +0700

    Setup lsp with mason

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+
+    "neovim/nvim-lspconfig",
+
+    {
+        "williamboman/mason.nvim",
+        config = function() 
+            require("mason").setup()
+        end, 
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependincies = {
+            "mason.nvim"
+        },
+        config = function()
+            local conf = require("mason-lspconfig")
+            conf.setup()
+            conf.setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end
+            })
+
+        end
+    },
+}
