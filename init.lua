a66fc82eff6bb063e930383f0e19ad7df74744ff
apakhomov <apkhmv@gmail.com>
2024-05-10 11:55:42 +0300

    Map J, K to move visual block

--- init.lua
+++ init.lua
@@ -36,3 +36,28 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
